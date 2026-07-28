1f0975352821fc9b0e9c1709740e39586bf18963
vak-rashu <rashu.cloud11@gmail.com>
2026-07-28 06:50:21 +0000

    Day 3- Solved Best time to Buy and Sell Stock - Used and learned two pointers approach

---
+++ arrays/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,17 @@
+# actual approach: using Two pointers
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        l, r = 0, 1
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                result = prices[r] - prices[l]
+                max_profit = max(max_profit, result)
+            else:
+                l = r
+            r += 1
+        return max_profit
+
+# time-complexity: O(n)
+# space-complexity: O(1)
