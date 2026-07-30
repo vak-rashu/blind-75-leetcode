77082e71b2ebd015197157573486d2c9c09de997
vak-rashu <rashu.cloud11@gmail.com>
2026-07-30 06:54:59 +0000

    A more simple Kadane

--- arrays/maximum_subarray.py
+++ arrays/maximum_subarray.py
@@ -17,16 +17,17 @@ class Solution:
 # space-complexity: O(1)
 
 # kadane's algorithm
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        max_sum = nums[0]
+        max_sum = float('-inf')
         curr_sum = 0
 
         for n in nums:
-            curr_sum = max(curr_sum, 0)
-            curr_sum += n
+            curr_sum = max(n, curr_sum + n)
             max_sum = max(max_sum, curr_sum)
         
         return max_sum
+
 # time-complexity: O(n)
 # space-complexity: O(1)
