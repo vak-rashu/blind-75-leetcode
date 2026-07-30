5a29ed82074adc3aaf7268a8455f50529efd67cc
vak-rashu <rashu.cloud11@gmail.com>
2026-07-30 06:40:41 +0000

    Solved Maximum Subarray problem using Kadane's Algorithm

--- arrays/maximum_subarray.py
+++ arrays/maximum_subarray.py
@@ -1,3 +1,5 @@
+# my appraoch- brute force method
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         max_sum = nums[0]
@@ -11,3 +13,20 @@ class Solution:
 
         return max_sum
 
+# time-complexity: O(n^2)
+# space-complexity: O(1)
+
+# kadane's algorithm
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = nums[0]
+        curr_sum = 0
+
+        for n in nums:
+            curr_sum = max(curr_sum, 0)
+            curr_sum += n
+            max_sum = max(max_sum, curr_sum)
+        
+        return max_sum
+# time-complexity: O(n)
+# space-complexity: O(1)
