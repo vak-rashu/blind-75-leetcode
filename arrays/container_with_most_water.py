76c9fec85b02c402ebf8ef117669134019db51b9
vak-rashu <rashu.cloud11@gmail.com>
2026-08-04 18:26:53 +0000

    Day 8: Solved Container with most water using Brute Force Method - Also used Optimal Solution using 2-pointers

--- arrays/container_with_most_water.py
+++ arrays/container_with_most_water.py
@@ -1,4 +1,4 @@
-#brute force method
+# brute force method
 
 class Solution:
     def maxArea(self, height: List[int]) -> int:
@@ -19,3 +19,31 @@ class Solution:
 # time-complexity: O(n^2)
 # space-complexity: O(1)
 
+# Optimal Solution
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        n = len(height)
+        area = 0
+
+        if n == 1:
+            return 0
+
+        l = 0
+        r = n - 1
+
+        while l < r:
+            curr_area = (r-l) * min(height[l], height[r])
+
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+
+            area = max(curr_area, area)
+
+        return area
+
+
+# time-complexity: O(n)
+# space-complexity: O(1)
