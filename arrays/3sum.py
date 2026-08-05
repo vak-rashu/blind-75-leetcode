39772fe694df8d03fc15ba679780db3506085f3e
vak-rashu <rashu.cloud11@gmail.com>
2026-08-05 18:08:21 +0000

    Solved 3sum using correct approach

--- arrays/3sum.py
+++ arrays/3sum.py
@@ -16,3 +16,31 @@ class Solution:
 
 # time-complexity: O(n^3)
 # space-complexity: O(n)
+
+
+# correct approach
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        val = []
+        nums.sort()
+
+        for i, a in enumerate(nums):
+            if i>0 and a == nums[i-1]:
+                continue
+
+            l, r = i+1, len(nums)-1
+            while l < r:
+                three_sum = a + nums[l] + nums[r]
+                if three_sum > 0:
+                    r-=1
+                elif three_sum < 0:
+                    l+=1
+                else:
+                    val.append([a, nums[l], nums[r]])
+                    l+=1
+                    while nums[l] == nums[l-1] and l < r:
+                        l+=1
+        return val
+
+# time-complexity: O(n^2)
+# space-complexity: O(1)
