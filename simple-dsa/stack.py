a486a61f28a28f3395337ed6d8840211c4c46a92
vak-rashu <rashu.cloud11@gmail.com>
2026-08-08 11:29:33 +0000

    DSA Implementation in Python using OOPS

---
+++ simple-dsa/stack.py
@@ -0,0 +1,35 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push_stack(self, ele):
+        self.stack.append(ele)
+
+    def pop_stack(self):
+
+        if len(self.stack) == 0:
+            return "Underflow Error: Cant evaluate items"
+
+        lastIndex = len(self.stack) - 1
+        popped_element = self.stack[lastIndex]
+        self.stack = self.stack[:lastIndex]
+
+        return (f"Popped element: {popped_element}\nNew Stack is: {self.stack}")
+
+def main():
+
+    a = Stack()
+    a.push_stack(5)
+    a.push_stack(4)
+    a.push_stack(6)
+    a.push_stack(7)
+    a.push_stack(8)
+
+    print(a.stack)
+
+    print(a.pop_stack())
+    print(a.pop_stack())
+
+    print(a.stack)
+
+main()
