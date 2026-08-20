548c97075aaf8d6c1e73b932714344d39e3548a7
vak-rashu <rashu.cloud11@gmail.com>
2026-08-20 18:30:25 +0000

    Two pointers for Merge Sorted Lists

--- general-learning/maths_for_dsa/tow_pointer.py
+++ general-learning/maths_for_dsa/tow_pointer.py
@@ -8,19 +8,35 @@
 #         print(array[r])
 #         r-=1
 
-def two_sum_two_pointers(array, target):
-    l, r = 0, len(array)-1
+# def two_sum_two_pointers(array, target):
+#     l, r = 0, len(array)-1
 
-    while l!=r:
-        target_sum = array[l] + array[r]
-        if target_sum == target:
-            return ([l, r], [array[l], array[r]])
-        elif target_sum > target:
-            r-=1
-        else:
+#     while l!=r:
+#         target_sum = array[l] + array[r]
+#         if target_sum == target:
+#             return ([l, r], [array[l], array[r]])
+#         elif target_sum > target:
+#             r-=1
+#         else:
+#             l+=1
+
+def two_pointers_merge_sorted_list(array1, array2):
+    l, r = 0, 0
+    n, m = len(array1), len(array2)
+    merged_array = []
+    min_val = 0
+
+    while l != n and r!= m:
+        min_val = min(array1[l], array2[r])
+        merged_array.append(min_val)
+
+        if min_val == array1[l]:
             l+=1
+        else:
+            r +=1
 
+    return merged_array
 
-array = [11, 4, 3, 2, 5]
-target = 6
-print(two_sum_two_pointers(array=array, target=target))
+array1 = [1, 3, 4]
+array2 = [-1, 0, 2]
+print(two_pointers_merge_sorted_list(array1, array2))
