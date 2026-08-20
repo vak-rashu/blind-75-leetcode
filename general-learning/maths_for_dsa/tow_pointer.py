7d24735e2bf2b8ec9d607aa30f9c1cedbca27657
vak-rashu <rashu.cloud11@gmail.com>
2026-08-20 18:10:59 +0000

    Two pointers for Two Sum

--- general-learning/maths_for_dsa/tow_pointer.py
+++ general-learning/maths_for_dsa/tow_pointer.py
@@ -1,12 +1,26 @@
-def two_pointers(array):
+# def two_pointers(array, target):
+#     target_sum = 0
+#     l, r = 0, len(array)-1
+
+#     while l!=r:
+#         print(array[l])
+#         l+=1
+#         print(array[r])
+#         r-=1
+
+def two_sum_two_pointers(array, target):
     l, r = 0, len(array)-1
 
-    while l<=r:
-        print(array[l])
-        l+=1
-        print(array[r])
-        r-=1
+    while l!=r:
+        target_sum = array[l] + array[r]
+        if target_sum == target:
+            return ([l, r], [array[l], array[r]])
+        elif target_sum > target:
+            r-=1
+        else:
+            l+=1
 
 
-array = [1, 2, 3, 4, 5]
-two_pointers(array=array)
+array = [11, 4, 3, 2, 5]
+target = 6
+print(two_sum_two_pointers(array=array, target=target))
